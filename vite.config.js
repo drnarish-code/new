bd2edeea672daba15cc43b1ff1d7c82de5477f29
drnarish-code <dr.narish@gmail.com>
2026-07-15 12:53:48 +0800

    Switch TV browser build minifier to built-in esbuild

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    // Transpile code to be compatible with older TV browsers (Chrome 47+)
-    target: 'es2015',
-    minify: 'esbuild', // Uses the built-in esbuild engine instead of requiring the optional 'terser' package
-    cssTarget: 'chrome47',
-  }
+  plugins: [react()]
 });
